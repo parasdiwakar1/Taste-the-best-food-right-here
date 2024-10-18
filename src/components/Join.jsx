44657cbb3fecaeb07973580559da289677c95bde
parasdiwakar1 <paras208017@gmail.com>
2024-10-18 11:55:32 +0530

    all food code

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -10,17 +10,17 @@ const Join = () => {
                   <div className="flex justify-center -space-x-2 isolate">
                      <img
                         className="relative z-30 inline-block rounded-full h-14 w-14 ring-4 ring-white"
-                        src="https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg"
+                        src="https://i.ibb.co/3Rx91t9/paras-a4.jpg"
                         alt="Dan_Abromov"
                      />
                      <img
                         className="relative z-20 inline-block rounded-full h-14 w-14 ring-4 ring-white"
-                        src="https://res.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_170,w_170,f_auto,g_faces,z_0.7,b_white,q_auto:eco,dpr_1/smokhfs2uevnppc2bmwl"
+                        src="https://i.ibb.co/dP6vQvJ/paras-img.jpg"
                         alt="Guillermo_Rauch"
                      />
                      <img
                         className="relative z-10 inline-block rounded-full h-14 w-14 ring-4 ring-white"
-                        src="https://leerob.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.b1d1472f.jpg&w=256&q=75"
+                        src="https://avatars.githubusercontent.com/u/166237181?v=4"
                         alt="Lee_Robinson"
                      />
                      <img
